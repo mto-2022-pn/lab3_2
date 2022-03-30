3c84869e3166ea651ead8bc8e658180b5ca86e82
Jakub Wlaźlak <kubikapb@gmail.com>
2022-03-30 10:43:44 +0200

    Clock added to Order.java

--- src/main/java/edu/iis/mto/time/Order.java
+++ src/main/java/edu/iis/mto/time/Order.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
+import java.time.Clock;
 
 public class Order {
 
@@ -11,8 +12,16 @@ public class Order {
     private State orderState;
     private List<OrderItem> items = new ArrayList<>();
     private LocalDateTime subbmitionDate;
+    private Clock clock;
 
     public Order() {
+
+        orderState = State.CREATED;
+        clock = Clock.systemDefaultZone();
+    }
+
+    public Order(Clock clock) {
+        this.clock = clock;
         orderState = State.CREATED;
     }
 
@@ -28,13 +37,13 @@ public class Order {
         requireState(State.CREATED);
 
         orderState = State.SUBMITTED;
-        subbmitionDate = LocalDateTime.now();
+        subbmitionDate = LocalDateTime.now(clock);
 
     }
 
     public void confirm() {
         requireState(State.SUBMITTED);
-        long hoursElapsedAfterSubmittion = subbmitionDate.until(LocalDateTime.now(), ChronoUnit.HOURS);
+        long hoursElapsedAfterSubmittion = subbmitionDate.until(LocalDateTime.now(clock), ChronoUnit.HOURS);
         if (hoursElapsedAfterSubmittion > VALID_PERIOD_HOURS) {
             orderState = State.CANCELLED;
             throw new OrderExpiredException();
