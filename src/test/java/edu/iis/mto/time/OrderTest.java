a8245c1c854fa6c79cc06b31ff214208f6c78622
Jakub Wlaźlak <kubikapb@gmail.com>
2022-03-30 10:51:33 +0200

    orderConfirmationAfterOrderExpireThrowsOrderExpiredException test added

--- src/test/java/edu/iis/mto/time/OrderTest.java
+++ src/test/java/edu/iis/mto/time/OrderTest.java
@@ -1,19 +1,41 @@
 package edu.iis.mto.time;
-
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.Clock;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
 
+@ExtendWith(MockitoExtension.class)
 class OrderTest {
 
-    @BeforeEach
-    void setUp() throws Exception {}
+    @Mock
+    private Clock clock;
+    private Order order;
 
-    @Test
-    void test() {
-        fail("Not yet implemented");
+    @BeforeEach
+    void setUp() throws Exception {
+        when(clock.getZone()).thenReturn(ZoneId.systemDefault());
+        order = new Order(clock);
     }
 
+    @Test
+    public void orderConfirmationAfterOrderExpireThrowsOrderExpiredException()
+    {
+        Instant orderSubmissionTime = Instant.now();
+        Instant orderConformationTime = orderSubmissionTime.plus(27,ChronoUnit.HOURS);
+
+        when(clock.instant()).thenReturn(orderSubmissionTime).thenReturn(orderConformationTime);
+        order.submit();
+        assertThrows(OrderExpiredException.class,()->order.confirm());
+        assertEquals(order.getOrderState(), Order.State.CANCELLED);
+
+    }
 }
