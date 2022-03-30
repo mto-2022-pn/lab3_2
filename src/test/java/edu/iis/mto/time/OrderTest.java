1fa234b30bd85246cbf5bbe394181fc9a304d674
Jakub Wlaźlak <kubikapb@gmail.com>
2022-03-30 11:08:08 +0200

    orderRealizeWithoutConfirmationThrowsOrderStateException test added and some minor code refactoring

--- src/test/java/edu/iis/mto/time/OrderTest.java
+++ src/test/java/edu/iis/mto/time/OrderTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.internal.matchers.Or;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.Clock;
@@ -28,8 +29,7 @@ class OrderTest {
     }
 
     @Test
-    void orderConfirmationAfterOrderExpireThrowsOrderExpiredException()
-    {
+    void orderConfirmationAfterOrderExpireThrowsOrderExpiredException() {
         Instant orderSubmissionTime = Instant.now();
         Instant orderConformationTime = orderSubmissionTime.plus(27,ChronoUnit.HOURS);
 
@@ -41,8 +41,7 @@ class OrderTest {
     }
 
     @Test
-    void orderConfirmationWithinAcceptedTimeChangeOrderState()
-    {
+    void orderConfirmationWithinAcceptedTimeChangeOrderState() {
         Instant orderSubmissionTime = Instant.now();
         Instant orderConformationTime = orderSubmissionTime.plus(4,ChronoUnit.HOURS);
 
@@ -51,12 +50,22 @@ class OrderTest {
         assertEquals(Order.State.SUBMITTED,order.getOrderState());
         order.confirm();
         assertEquals(Order.State.CONFIRMED, order.getOrderState());
+        order.realize();
+        assertEquals(Order.State.REALIZED, order.getOrderState());
 
     }
+
     @Test
-    void orderConfirmationWithoutSubmitThrowsOrderStateException()
-    {
+    void orderConfirmationWithoutSubmitThrowsOrderStateException() {
         assertThrows(OrderStateException.class,()->order.confirm());
     }
 
+    @Test
+    void orderRealizeWithoutConfirmationThrowsOrderStateException(){
+        Instant orderSubmissionTime = Instant.now();
+        when(clock.instant()).thenReturn(orderSubmissionTime);
+        order.submit();
+        assertThrows(OrderStateException.class,()->order.realize());
+
+    }
 }
