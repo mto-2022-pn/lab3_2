ee07ad612f6bdc9c4def0b131e634639d293806f
Adrian <adrian.pietrz@gmail.com>
2022-04-01 20:49:32 +0200

    utworzenie testu z makietą upływu czasu

--- src/test/java/edu/iis/mto/time/OrderTest.java
+++ src/test/java/edu/iis/mto/time/OrderTest.java
@@ -1,19 +1,53 @@
 package edu.iis.mto.time;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.lenient;
+import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.Clock;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 
 
+
+
+@ExtendWith(MockitoExtension.class)
 class OrderTest {
 
+    @Mock
+    private Clock clock;
+
+    private Order order;
+
     @BeforeEach
-    void setUp() throws Exception {}
+    public void setUp() throws Exception {
+        lenient().when(clock.getZone()).thenReturn(ZoneId.systemDefault());
+        order = new Order(clock);
+
+    }
 
     @Test
-    void test() {
-        fail("Not yet implemented");
+    public void methodShouldThrowAnExceptionWhenOrderIsExpired() {
+        ZoneId zoneId = ZoneId.systemDefault();
+        LocalDateTime time = LocalDateTime.now();
+        when(clock.instant()).thenReturn(time.atZone(zoneId).toInstant());
+        order.submit();
+
+
+        time = LocalDateTime.now().plusDays(2);
+        when(clock.instant()).thenReturn(time.atZone(zoneId).toInstant());
+
+
+        assertThrows(OrderExpiredException.class, () -> order.confirm());
     }
+
+    
 
 }
