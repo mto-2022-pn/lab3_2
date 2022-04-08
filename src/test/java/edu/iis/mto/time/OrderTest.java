5ccba32c69ec965b12827bda1f7eca89b2500c90
Norbert Łodziński <209382@edu.p.lodz.pl>
2022-04-08 21:33:37 +0200

    added test:  hoursElapsedGreaterThanValidPeriodHours_shouldThrowOrderExpiredException

--- src/test/java/edu/iis/mto/time/OrderTest.java
+++ src/test/java/edu/iis/mto/time/OrderTest.java
@@ -1,19 +1,39 @@
 package edu.iis.mto.time;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.*;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalUnit;
 
+@ExtendWith(MockitoExtension.class)
 class OrderTest {
+    @Mock
+    Clock clock;
+    Order order;
 
     @BeforeEach
-    void setUp() throws Exception {}
-
-    @Test
-    void test() {
-        fail("Not yet implemented");
+    void setUp() throws Exception {
+        when(clock.getZone()).thenReturn(ZoneId.systemDefault());
+        order = new Order(clock);
     }
 
+    @Test
+    void hoursElapsedGreaterThanValidPeriodHours_shouldThrowOrderExpiredException() {
+        Instant submissionDate = Instant.now();
+        Instant confirmationDate = submissionDate.plus(25, ChronoUnit.HOURS);
+
+        when(clock.instant())
+                .thenReturn(submissionDate)
+                .thenReturn(confirmationDate);
+        order.submit();
+        assertThrows(OrderExpiredException.class, () -> order.confirm());
+    }
 }
