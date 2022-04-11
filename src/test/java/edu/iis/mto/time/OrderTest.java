e3e1cc6b13c6a472accfd4f3696340796dacf213
Konrad Basta <konrad.basta@onet.com.pl>
2022-04-11 18:34:49 +0200

    Added Order Expired tests

--- src/test/java/edu/iis/mto/time/OrderTest.java
+++ src/test/java/edu/iis/mto/time/OrderTest.java
@@ -5,15 +5,39 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDateTime;
+
 
 class OrderTest {
 
-    @BeforeEach
-    void setUp() throws Exception {}
+    private static final long VALID_PERIOD_HOURS = 24;
+    private Order order;
+    private MockUpClock clock = new MockUpClock();
 
-    @Test
-    void test() {
-        fail("Not yet implemented");
+    @BeforeEach
+    void setUp() throws Exception {
+        clock.setTime(LocalDateTime.now());
+        order = new Order(clock);
     }
 
+    @Test
+    void orderNotExpiredTest() {
+        order.submit();
+        clock.setTime(LocalDateTime.now().plusHours(VALID_PERIOD_HOURS - 1));
+        assertDoesNotThrow(() -> order.confirm());
+    }
+
+    @Test
+    void orderExpiredTest() {
+        order.submit();
+        clock.setTime(LocalDateTime.now().plusHours(VALID_PERIOD_HOURS));
+        assertThrows(OrderExpiredException.class, () -> order.confirm());
+    }
+
+    @Test
+    void orderExpiredOneHourTest() {
+        order.submit();
+        clock.setTime(LocalDateTime.now().plusHours(VALID_PERIOD_HOURS + 1));
+        assertThrows(OrderExpiredException.class, () -> order.confirm());
+    }
 }
