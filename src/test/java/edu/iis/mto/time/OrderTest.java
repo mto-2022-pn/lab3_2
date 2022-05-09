7d6964880d712d96703e6c1dbee31d55e9297762
Wojciech Farej <228730@edu.p.lodz.pl>
2022-05-09 18:18:10 +0200

    added tests

--- src/test/java/edu/iis/mto/time/OrderTest.java
+++ src/test/java/edu/iis/mto/time/OrderTest.java
@@ -2,18 +2,34 @@ package edu.iis.mto.time;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.time.LocalDateTime;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-
 class OrderTest {
 
+    private MockClock clock;
+    private Order order;
+
     @BeforeEach
-    void setUp() throws Exception {}
+    void setUp() throws Exception {
+        clock = new MockClock();
+        order = new Order(clock);
+    }
 
     @Test
-    void test() {
-        fail("Not yet implemented");
+    void orderDidntExpired() {
+        order.submit();
+        clock.setTime(LocalDateTime.now().plusHours(13));
+        assertDoesNotThrow(() -> order.confirm());
+    }
+
+    @Test
+    void orderExpired() {
+        order.submit();
+        clock.setTime(LocalDateTime.now().plusHours(25));
+        assertThrows(OrderExpiredException.class, () -> order.confirm());
     }
 
 }
