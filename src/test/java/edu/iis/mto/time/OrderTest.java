d4ae939c5b8e79079f2ff7d901d686aa5639fbe1
Karol Profic <karolprofic@protonmail.com>
2022-05-19 22:48:55 +0200

    Added tests

--- src/test/java/edu/iis/mto/time/OrderTest.java
+++ src/test/java/edu/iis/mto/time/OrderTest.java
@@ -1,19 +1,54 @@
 package edu.iis.mto.time;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import java.time.Clock;
+import java.time.Duration;
+import java.time.Instant;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
+public class OrderTest {
 
-class OrderTest {
-
-    @BeforeEach
-    void setUp() throws Exception {}
+    private Clock getClockMockWithTimeWithAddedOffset(Duration offsetDuration) {
+        Clock clock = mock(Clock.class);
+        Instant submitTime = Instant.now();
+        Instant confirmTime = submitTime.plus(offsetDuration);
+        when(clock.instant()).thenReturn(submitTime, confirmTime);
+        return clock;
+    }
 
     @Test
-    void test() {
-        fail("Not yet implemented");
+    public void orderNotExpiredInstantly() {
+        Clock clock = getClockMockWithTimeWithAddedOffset(Duration.ofHours(0));
+        Order order = new Order(clock);
+        order.submit();
+        assertDoesNotThrow(order::confirm);
+    }
+
+    @Test
+    public void orderNotExpiredAfterHoursLessThanValidPeriod() {
+        Clock clock = getClockMockWithTimeWithAddedOffset(Duration.ofHours(8));
+        Order order = new Order(clock);
+        order.submit();
+        assertDoesNotThrow(order::confirm);
+    }
+
+    @Test
+    public void orderNotExpiredOnValidPeriod() {
+        Clock clock = getClockMockWithTimeWithAddedOffset(Duration.ofHours(24));
+        Order order = new Order(clock);
+        order.submit();
+        assertDoesNotThrow(order::confirm);
+    }
+
+    @Test
+    public void orderExpiredOnHoursMoreThanValidPeriod() {
+        Clock clock = getClockMockWithTimeWithAddedOffset(Duration.ofHours(25));
+        Order order = new Order(clock);
+        order.submit();
+        assertThrows(OrderExpiredException.class, order::confirm);
     }
 
 }
